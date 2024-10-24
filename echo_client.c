bdadd7fe0d8dc150e3e902d7b7d7b49fdd569617
Peter Chang <peter.yaochen.chang@gmail.com>
2024-10-24 16:37:37 +0800

    Part 1: echo server/client

---
+++ echo_client.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE] = {0};
+
+    // 1. Create socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        printf("Socket creation error\n");
+        return -1;
+    }
+
+    // 2. Define the server address
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 address from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("Invalid address/ Address not supported\n");
+        return -1;
+    }
+
+    // 3. Connect to the server
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("Connection failed\n");
+        return -1;
+    }
+
+    // 4. Send message to server
+    char *message = "Hello from client\n";
+    send(sock, message, strlen(message), 0);
+    printf("Message sent to server\n");
+
+    // 5. Receive echoed message from server
+    int valread = read(sock, buffer, BUFFER_SIZE);
+    printf("Echoed message from server: %s\n", buffer);
+
+    // 6. Close the socket
+    close(sock);
+    return 0;
+}
